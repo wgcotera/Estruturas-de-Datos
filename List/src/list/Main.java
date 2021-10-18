465ec630bb1fabdf82dd18174196f031abdb29ba
wgcotera <wgcotera@espol.edu.ec>
2021-10-17 20:18:08 -0500

    Inicio: Implementacion de TDA List

---
+++ List/src/list/Main.java
@@ -0,0 +1,16 @@
+package list;
+
+/**
+ *
+ * @author wgcotera
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
